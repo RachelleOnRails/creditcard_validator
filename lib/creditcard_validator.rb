1d59c7e98cc4a24068db6126bd3a80de75d03e38
Rachelle Le Quesne <Rachelle.LeQuesne@gmail.com>
2014-06-05 15:21:24 +1000

    Added invalid input checks

--- lib/creditcard_validator.rb
+++ lib/creditcard_validator.rb
@@ -1,3 +1,33 @@
+class InvalidInput < StandardError
+end
+
 class CreditcardValidator
 
+  def self.valid_input?(input)
+    input.to_s[/^[0-9]\d*$/]
+  end
+
+  def self.call(number)
+    if valid_input? number
+      puts "#{number} is valid"
+    else
+      raise InvalidInput
+    end
+  end
+
+  def self.get_input
+    puts "Enter a credit card number: "
+    STDOUT.flush
+    gets.chomp
+  end
+
+end
+
+if __FILE__ == $0
+  n = CreditcardValidator.get_input
+  begin
+    CreditcardValidator.call(n)
+  rescue InvalidInput
+    puts "Invalid input. Only numbers are accepted."
+  end
 end
