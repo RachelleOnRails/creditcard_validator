3247083589bd29e9fee041eca12e31b12759d6c4
Rachelle Le Quesne <Rachelle.LeQuesne@gmail.com>
2014-06-05 14:58:04 +1000

    Add rspec file

---
+++ spec/creditcard_validator_spec.rb
@@ -0,0 +1,14 @@
+require_relative '../lib/creditcard_validator'
+
+describe 'creditcard_validator' do
+
+  context 'valid input' do
+  end
+
+  context 'invalid input' do
+  end
+
+  context 'really crappy input' do
+  end
+
+end
