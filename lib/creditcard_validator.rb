1b56749396724a1c251222c1c95524927e241d6d
Rachelle Le Quesne <Rachelle.LeQuesne@gmail.com>
2014-06-05 15:34:54 +1000

    Added card type

--- lib/creditcard_validator.rb
+++ lib/creditcard_validator.rb
@@ -7,9 +7,50 @@ class CreditcardValidator
     input.to_s[/^[0-9]\d*$/]
   end
 
+  # +============+=============+===============+
+  # | Card Type  | Begins With | Number Length |
+  # +============+=============+===============+
+  # | AMEX       | 34 or 37    | 15            |
+  # +------------+-------------+---------------+
+  # | Discover   | 6011        | 16            |
+  # +------------+-------------+---------------+
+  # | MasterCard | 51-55       | 16            |
+  # +------------+-------------+---------------+
+  # | Visa       | 4           | 13 or 16      |
+  # +------------+-------------+---------------+
+  def self.card_type(number)
+    case number.length
+      when 13
+        if number.start_with?('4')
+          'VISA'
+        else
+          'Unknown'
+        end
+      when 15
+        if number.start_with?('34') || number.start_with?('37')
+          'AMEX'
+        else
+          'Unknown'
+        end
+      when 16
+        if number.start_with?('6011')
+          'Discover'
+        elsif number.start_with?('51') || number.start_with?('52') || number.start_with?('53') || number.start_with?('54') || number.start_with?('55')
+          'MasterCard'
+        elsif number.start_with?('4')
+          'VISA'
+        else
+          'Unknown'
+        end
+      else
+        'Unknown'
+    end
+  end
+
   def self.call(number)
     if valid_input? number
-      puts "#{number} is valid"
+      card_type = card_type(number)
+      puts "card type is #{card_type}"
     else
       raise InvalidInput
     end
